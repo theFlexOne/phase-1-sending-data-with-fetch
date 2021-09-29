e789f2b15f9123bfa9469af4e6e942e64f18ce15
Eric Hafvenstein <edhavey@null.net>
2021-09-29 00:25:00 -0500

    assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Add your code here
+const body = document.querySelector('body')
+
+const submitData = (name, email) => {
+  const formData = {
+    name,
+    email
+  };
+  const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(formData),
+  };
+
+  return fetch("http://localhost:3000/users", configurationObject)
+    .then(res => res.json())
+    .then(data => body.textContent = data.id)
+    .catch((error) => body.textContent = error.message)
+};
